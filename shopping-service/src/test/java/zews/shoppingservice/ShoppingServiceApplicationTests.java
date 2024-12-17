9449a72a9a5877ae39540abf339f544e330ecb5b
Janko <jankoristic@proton.me>
2024-12-17 15:40:59 +0100

    two new modules added, email & shopping module

---
+++ shopping-service/src/test/java/zews/shoppingservice/ShoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zews.shoppingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
