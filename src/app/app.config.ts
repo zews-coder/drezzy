738e909bca0d5e2b5041249a7e2fd3c0dbd9d1b7
Janko <jankoristic@proton.me>
2025-02-04 20:40:24 +0100

    statistic view added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
--- src/app/components/vendor/stats/stats.component.ts
+++ src/app/components/vendor/stats/stats.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VendorHomeComponent } from "../vendor-home/vendor-home.component";
+import { HttpClient } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts'
 
 @Component({
   selector: 'app-stats',
-  imports: [VendorHomeComponent],
+  imports: [VendorHomeComponent, NgxChartsModule],
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.css'
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit{
+  earnings: number = 0;
+  
+  itemsSoldData: any[] = [];
+  ordersData: any[] = [];
+
+  view: [number, number] = [500, 300]; // Chart size
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchEarnings();
+    this.fetchItemsSold();
+    this.fetchOrdersByStatus();
+  }
+
+  fetchEarnings() {
+    this.http.get<number>('http://localhost:9090/api/v1/stats/earnings').subscribe(
+      (data) => (this.earnings = data),
+      (error) => console.error('Error fetching earnings', error)
+    );
+  }
+
+  fetchItemsSold() {
+    this.http.get<any>('http://localhost:9090/api/v1/stats/itemsSold').subscribe(
+      (data) => {
+        this.itemsSoldData = [
+          { name: 'Clothes', value: data.clothesSold },
+          { name: 'Shoes', value: data.shoesSold }
+        ];
+      },
+      (error) => console.error('Error fetching items sold', error)
+    );
+  }
+
+  fetchOrdersByStatus() {
+    this.http.get<any>('http://localhost:9090/api/v1/stats/billsByStats').subscribe(
+      (data) => {
+        this.ordersData = [
+          { name: 'PAID', value: data.ordersPaid },
+          { name: 'SHIPPED', value: data.ordersShipped },
+          { name: 'DELIVERED', value: data.ordersDelivered }
+        ];
+      },
+      (error) => console.error('Error fetching orders by status', error)
+    );
+  }
 
 }
